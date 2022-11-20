797ea83b6d0e9f91c41560fc2c0c58242746aab3
Yury Amonsky <yury.amonsky@yandex.ru>
2022-11-20 10:40:13 +0300

    feat: поменять baseUrl b allowedCORS на рабочие домены

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,9 +1,8 @@
 const OK = 200;
 const URL_PATTERN = /^https?:\/\/(?:w{3}\.)?(?:[a-z0-9]+[a-z0-9-]*\.)+[a-z]{2,}(?::[0-9]+)?(?:\/\S*)?#?$/i;
 const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
   'http://localhost:3001',
+  'http://amo.edu.nomoredomains.club',
 ];
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports = {
--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -31,7 +31,7 @@
     # Если передан некорректный токен
     401 — Переданный токен некорректен
  */
-export const baseUrl = 'http://localhost:3000'; // https://auth.nomoreparties.co';
+export const baseUrl = 'http://api.amo.edu.nomoredomains.club';
 
 const request = ({
   url,
